b4f3e63d494b4cdab70723122f50556261ccd0c4
sergiu-paraschiv <sergiuspk@gmail.com>
2014-05-10 20:38:15 +0300

    added buildsystem, fixed gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,17 +29,61 @@ module.exports = function(grunt) {
                 path: 'http://localhost:9001/',
                 app: 'Chrome'
             }
+        },
+
+        useminPrepare: {
+            html: 'views/index.html',
+            options: {
+                root: '',
+                dest: 'build'
+            }
+        },
+  
+        usemin: {
+            html: ['build/views/*.html'],
+            options: {
+                assetsDirs: ['styles', '.tmp'],
+                dest: 'build'
+            }
+        },
+
+        copy: {
+            beforeBuild: {
+                files: [                    
+                    { expand: true, flatten: true, src: ['views/*.html'], dest: 'build/views/'}
+                ]
+            },
+
+            afterBuild: {
+                files: [
+                    { expand: true, flatten: true, src: ['images/*'], dest: 'build/images/' },
+                    { expand: true, flatten: true, src: ['images/content/*'], dest: 'build/images/content/' },
+                    { expand: true, flatten: true, src: ['.tmp/concat/styles/*'], dest: 'build/styles/' }
+                ]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-usemin');
     grunt.loadNpmTasks('grunt-open');
     
     grunt.registerTask('default', [
         'connect',
         'open',
         'watch'
+    ]);
+
+    grunt.registerTask('build', [
+        'copy:beforeBuild',
+        'useminPrepare',
+        'concat',
+        'usemin',
+        'copy:afterBuild'
     ]);
 
 };
